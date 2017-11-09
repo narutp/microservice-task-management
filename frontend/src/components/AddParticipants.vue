02e6b04f79c941c631a27b8770e5fd69e9ec9bd2
Narut Poovorakit <net.narut.p@gmail.com>
2017-11-09 16:00:51 +0700

    Try to add external paricipant in card

--- frontend/src/components/AddParticipants.vue
+++ frontend/src/components/AddParticipants.vue
@@ -12,6 +12,7 @@
       </div>
     </div>
 
+    <!--  Internal Participants -->
     <div class="columns">
       <div class="column">
         <input v-model="temp" class="input" type="text" placeholder="Card name">
@@ -19,9 +20,9 @@
 
       <div class="column">
         <section class="my-task-table--body">
-          <b-select v-model="user" placeholder="Select a name">
+          <b-select v-model="internalUser" placeholder="Select a name">
             <option
-              v-for="option in userList"
+              v-for="option in internalUserList"
               :value="option"
               :key="option">
               {{ option }}
@@ -30,19 +31,53 @@
         </section>
       </div>
       <div class="column">
-        <button class="button is-info" @click="addParticipant()">
-          <span>Add Participants</span>
+        <button class="button is-info" @click="addInternalParticipant()">
+          <span>Add Internal Participants</span>
         </button>
       </div>
     </div>
 
-    <section class="my-task-table--body">
+    <!--  external participants  -->
+    <div class="columns">
+      <div class="column">
+        <input v-model="temp2" class="input" type="text" placeholder="Card name">
+      </div>
+
+      <div class="column">
+        <section class="my-task-table--body">
+          <b-select v-model="externalUser" placeholder="Select external">
+            <option
+              v-for="options in externalUserList"
+              :value="options"
+              :key="options">
+              {{ options }}
+            </option>
+          </b-select>
+        </section>
+      </div>
+      <div class="column">
+        <button class="button is-info" @click="addExternalParticipant()">
+          <span>Add External Participants</span>
+        </button>
+      </div>
+    </div>
+
+    <section class="add-participants-tag--body">
       <el-tag
-        v-for="tag in addList"
+        v-for="tag in internalAddList"
         :key="tag"
         closable
         :type="tag">
         {{tag}}
+      </el-tag>
+    </section>
+    <section class="add-participants-tag--body">
+      <el-tag
+        v-for="tags in externalAddList"
+        :key="tags"
+        closable
+        :type="tags">
+        {{tags}}
       </el-tag>
     </section>
     <br>
@@ -60,27 +95,40 @@ export default {
       isPaginated: true,
       isPaginationSimple: false,
       temp: '',
-      user: '',
-      userList: [],
-      addList: []
+      temp2: '',
+      internalUser: '',
+      internalUserList: [],
+      internalAddList: [],
+      externalUser: '',
+      externalUserList: [],
+      externalAddList: []
     }
   },
-  // TODO can't get data in select
-  async beforeCreate () {
+  // TODO can't get data in select at first
+  async mounted () {
     let idDepartment = localStorage.getItem('id_department_owner_card')
-    let response = await Axios.get(`http://localhost:8090/get/internal-user-list/department/${idDepartment}`)
-    for (let i = 0; i < response.data.length; i++) {
-      this.userList[i] = response.data[i].name
+    let internalResponse = await Axios.get(`http://localhost:8090/get/internal-user-list/department/${idDepartment}`)
+    for (let i = 0; i < internalResponse.data.length; i++) {
+      this.internalUserList[i] = internalResponse.data[i].name
     }
+
+    let externalResponse = await Axios.get(`http://localhost:8090/get/external-user-list/department/${idDepartment}`)
+    for (let j = 0; j < externalResponse.data.length; j++) {
+      this.externalUserList[j] = externalResponse.data[j].name
+    }
+    console.log(this.externalUserList)
   },
   methods: {
-    addParticipant () {
-      this.addList.push(this.user)
+    addInternalParticipant () {
+      this.internalAddList.push(this.internalUser)
+    },
+    addExternalParticipant () {
+      this.externalAddList.push(this.externalUser)
     },
     cancle () {
     },
     async save () {
-      let idUserListResponse = await Axios.get(`http://localhost:8090/get/idUserList/nameList/${this.addList}`)
+      let idUserListResponse = await Axios.get(`http://localhost:8090/get/idUserList/nameList/${this.internalAddList}`)
       console.log(idUserListResponse.data)
       let idCard = localStorage.getItem('id_create_card')
       let response = await Axios.post(`http://localhost:8091/add/internal-participants/${idCard}/${idUserListResponse.data}`)
