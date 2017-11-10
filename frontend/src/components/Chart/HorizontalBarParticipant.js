866b41756ce85adc2f4e0e84dae4a66a0699c830
Makhamwan <mkhv.bt96@gmail.com>
2017-11-10 09:05:04 +0900

    modify dashboard

--- frontend/src/components/Chart/HorizontalBarParticipant.js
+++ frontend/src/components/Chart/HorizontalBarParticipant.js
@@ -1,21 +1,16 @@
-import { HorizontalBar } from 'vue-chartjs'
+import { Bar } from 'vue-chartjs'
 // import the component - chart you need
 
-export default HorizontalBar.extend({
+export default Bar.extend({
   mounted () {
     // Overwriting base render method with actual data.
     this.renderChart({
       labels: ['Department A', 'Department B', 'Department C', 'Department D'],
       datasets: [
         {
-          label: ['Internal Participants'],
-          backgroundColor: '#FF6384',
+          label: ['Participants'],
+          backgroundColor: '#585858',
           data: [12, 20, 12, 18]
-        },
-        {
-          label: ['External Participants'],
-          backgroundColor: '#36A2EB',
-          data: [4, 3, 1, 2]
         }
       ]
     })
