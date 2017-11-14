d501e18d743ec47bf02444beb803402985d9b8a1
Makhamwan <mkhv.bt96@gmail.com>
2017-11-15 07:02:22 +0900

    get department value

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -4,17 +4,48 @@
       <br>
       <el-col :span="11">
         <h1><b> Organization </b></h1><br>
+
         <b-table
-            class="home--table"
+            class="task-management--table"
             :data="tableUserData"
+            :paginated="true"
+            :per-page="7"
             default-sort="title">
+
             <template scope="props">
-              <b-table-column field="Department" label="Department" width="50" sortable>
-                  {{ props.row.department }}
-              </b-table-column>
-              <b-table-column field="Project" label="Total Internal" width="50" sortable>
-                  {{ props.row.internal }}
-              </b-table-column>
+                <b-table-column field="department" label="Department" sortable>
+                    {{ props.row.name }}
+                </b-table-column>
+
+                <b-table-column field="numUser" label="Internal" sortable>
+                    {{ props.row.numUser }}
+                </b-table-column>
+            </template>
+        </b-table>
+
+        <b-table
+            class="task-management--table"
+            :data="tableProjectData"
+            :paginated="true"
+            :per-page="7"
+            default-sort="title">
+
+            <template scope="props">
+                <b-table-column field="no" label="No" width="50" sortable numeric centered>
+                    {{ props.row.no }}
+                </b-table-column>
+
+                <b-table-column field="projectName" label="Project Name" sortable>
+                    {{ props.row.name }}
+                </b-table-column>
+
+                <b-table-column field="registeredDate" label="Registered Date" sortable>
+                    {{ props.row.registeredDate }}
+                </b-table-column>
+
+                <b-table-column field="owner" label="Owner" sortable>
+                    {{ props.row.idUser }}
+                </b-table-column>
             </template>
         </b-table>
         <modal-person> </modal-person>
@@ -53,18 +84,35 @@
 // import { mapActions } from 'vuex'
 import ModalTask from '@/components/ModalTask'
 import ModalPerson from '@/components/ModalPerson'
+import Axios from 'axios'
 export default {
   data () {
     return {
+      arrLength: 0,
       dialogVisible: false,
-      tableProjectData: [{ 'department': 'A', 'project': 3, 'taskCard': 5 },
-      { 'department': 'B', 'project': 13, 'taskCard': 15 },
-      { 'department': 'C', 'project': 23, 'taskCard': 25 },
-      { 'department': 'D', 'project': 7, 'taskCard': 16 }],
-      tableUserData: [{ 'department': 'A', 'internal': 13, 'external': 5 },
-      { 'department': 'B', 'internal': 15, 'external': 5 },
-      { 'department': 'C', 'internal': 12, 'external': 5 },
-      { 'department': 'D', 'internal': 18, 'external': 6 }]
+      tableUserData: [{ 'name': 'DapartmentName', 'numUser': 'Internal' }],
+      tableProjectData: [{ 'department': 'A', 'project': 3, 'taskCard': 5 }]
+    }
+  },
+  async mounted () {
+    // let response = await Axios.get(`http://localhost:8091/get/all-project/`)
+    // this.tableUserData = response.data
+    // this.arrLength = response.data.length
+    // for (let i = 0; i < this.arrLength; i++) {
+    //   let id = response.data[i].idUser
+    //   let nameResponse = await Axios.get(`http://localhost:8090/get/user/id/${id}`)
+    //   console.log(nameResponse)
+    //   this.tableUserData[i].idUser = nameResponse.data.name
+    // }
+    // user
+    let response = await Axios.get(`http://localhost:8090/get/all-department`)
+    this.tableUserData = response.data
+    this.arrLength = response.data.length
+    for (let i = 0; i < this.arrLength; i++) {
+      let id = response.data[i].idDepartment
+      let numResponse = await Axios.get(`http://localhost:8090/get/internal-user-list/department/${id}`)
+      console.log(numResponse)
+      this.tableUserData[i].numUser = numResponse.data.length
     }
   },
   computed: {
